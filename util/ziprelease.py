de3a6ff7356bfd8f0e05976037be4714cddf5281
= <=>
2013-10-02 09:13:47 -0500

    Added a tool to create a zipfile for release
    To push to the vimscripts site.

---
+++ util/ziprelease.py
@@ -0,0 +1,19 @@
+import zipfile
+import os
+
+# run from $VIMPROCESSING/utils -- fix later
+os.chdir("..")
+fname = "processing.zip"
+
+
+zf = zipfile.ZipFile(fname, 'w')
+
+#top level - don't include anything but the dirs
+walker = os.walk('.')
+root, dirs, files = walker.next()
+for root, dirs, files in walker:
+    for f in files:
+        addfile = os.path.join(root, f)
+        if not ".git" in addfile:
+            zf.write(addfile)
+zf.close()
